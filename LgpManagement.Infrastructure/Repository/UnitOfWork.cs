1e2a619fdb8a7cdb3cc5eaee60f353a950baa7d5
roy-subrata <roy.subrata.cs@gmail.com>
2020-09-12 00:46:56 +0600

    added UnitOf Work Functionality

---
+++ LgpManagement.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using LpgManagement.Core.Repository;
+using LpgManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LgpManagement.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LgpContext _context;
+        public UnitOfWork(LgpContext context)
+        {
+            _context = context;
+            Category = new CategoryRepository(_context);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public int Commit()
+        {
+           return _context.SaveChanges();
+        }
+       
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ LpgManagement.Core/Repository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LpgManagement.Core.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        int Commit();
+    }
+}
