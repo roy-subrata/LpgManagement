fd82fec9c0ddfe482a70bec7982e05fe438b126c
roy-subrata <roy.subrata.cs@gmail.com>
2020-09-11 23:33:52 +0600

    added category repository functionality

---
+++ LpgManagement.Core/Repository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using LpgManagement.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LpgManagement.Core.Repository
+{
+   public interface ICategoryRepository
+    {
+
+    }
+}
